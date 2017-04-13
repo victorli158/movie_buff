6d690ef3979498f1f4155f79ee16fc068a375028
Victor Li <victor.li158@gmail.com>
2017-04-13 10:45:09 -0700

    Add eighties_b_movies method

--- movie_buff/02_queries.rb
+++ movie_buff/02_queries.rb
@@ -1,7 +1,9 @@
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where("yr BETWEEN 1980 AND 1989 AND score >= 3 AND score <= 5")
 end
 
 def bad_years
